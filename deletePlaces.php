df110eea14ec734546d4b4b3715b31aecb625f0d
thanhduy1 <tranthanhduytvh10@gmail.com>
2022-07-26 16:03:35 +0700

    first commit

--- deletePlaces.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include("connection.php");
-
-    $chiNhanh=$_GET['iddd'];
-
-    $query_delete_diadiem = "DELETE FROM tbl_diadiem_chinhanh where maChiNhanh = '$chiNhanh'";
-    $result_delete_diadiem = $conn->query($query_delete_diadiem);
-    header('location:pagePlaces.php');  
-?>
--- deleteProducts.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("connection.php");
-
-    $maSanPham=$_GET['idth'];
-    $query_delete_SanPham = "DELETE FROM tbl_sanpham where maSanPham = '$maSanPham'";
-    $result_delete_SanPham = $conn->query($query_delete_SanPham);
-    header('location:pageProducts.php');
-?>
