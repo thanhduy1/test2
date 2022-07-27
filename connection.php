695e520bc57a4c2a4188a9401ac583fafbc42fac
thanhduy1 <tranthanhduytvh10@gmail.com>
2022-07-27 22:04:02 +0700

    first commit

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     $server_name = "sql6.freemysqlhosting.net";
-    $username = "sql6508901";
-    $password = "y6WtmIgF7f";
-    $database_name = "sql6508901";
+    $username = "sql6509179";
+    $password = "53Lilcmc8k";
+    $database_name = "sql6509179";
     $conn = mysqli_connect($server_name, $username, $password, $database_name);
     if (!$conn) {
             echo "Error " ;
